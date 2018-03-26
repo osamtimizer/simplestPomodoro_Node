43318676546b469b27df9b0a4e2846623363c96e
osamtimizer <eaten.jb@gmail.com>
2018-03-26 11:01:07 +0900

    Add function to display activity fetched from RealtimeDB

--- public/javascripts/activity.js
+++ public/javascripts/activity.js
@@ -14,51 +14,99 @@ firebase.initializeApp(config);
 const auth = firebase.auth();
 const database = firebase.database();
 
+const DATE_YMD_FORMAT = "YYYY-MM-DD";
+
 $(() => {
   auth.onAuthStateChanged((user) => {
     if (user) {
       const uid = user.uid;
       console.log(uid);
-      const today = moment().format("YYYY-MM-DD");
-      const one_week = moment.duration(1, 'weeks');
-      const activity = fetchAppropriateActivity(user.uid, today, one_week);
+      const today = moment();
+      const one_week = moment.duration(6, 'days');
+      const startDate = moment(today - one_week);
+      const endDate = moment(today + moment.duration(1, 'days'));
+      fetchAppropriateActivity(user.uid, today, one_week)
+      .then((result) => {
+        console.log("return value from fetch method: ", result);
+        let labels = [];
+        let data = [];
+
+        for(var i = startDate; i.isBefore(endDate); i.add('days', 1)) {
+          console.log(i.format(DATE_YMD_FORMAT));
+          labels.push(i.format(DATE_YMD_FORMAT));
+
+          let isFound = false;
+          result.forEach((item) => {
+            if (i.format(DATE_YMD_FORMAT) === item.date) {
+              data.push(item.count);
+              isFound = true;
+            }
+          });
+          if (!isFound) {
+            data.push(0);
+          }
+        }
+        console.log("labels: ", labels);
+        console.log("data: ", data);
+
+        //draw chart
+        //If you want to add graph on the canvas, just add item to "datasets"
+        const context = $('#myChart')[0].getContext('2d');
+        const myChart = new Chart(context, {
+          type: 'bar',
+          data: {
+            labels: labels,
+            datasets: [{
+              label: 'activity',
+              data: data,
+              backgroundColor: 'rgba(153,255,51,0.4)'
+            }]
+          }
+        });
+      }).catch((err) => {
+        console.error(err);
+      });
     }
   });
 
-
-  //draw chart
-  const context = $('#myChart')[0].getContext('2d');
-  const myChart = new Chart(context, {
-    type: 'bar',
-    data: {
-      labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-      datasets: [{
-        label: 'apples',
-        data: [12, 19, 3, 17, 6, 3, 7],
-        backgroundColor: 'rgba(153,255,51,0,4)'
-      }, {
-        label: 'oranges',
-        data: [2, 29, 5, 5, 2, 3, 10],
-        backgroundColor: 'rgba(255,153,0,0,4)'
-      }]
-    }
-  });
 });
 
 const fetchAppropriateActivity = (uid, targetDate, duration) => {
-  let activities = [];
-  const ref = database.ref('users/' + uid + '/result');
-  const days = duration.days();
-  console.log("fetchAppropriateActivity");
-  ref.once("value").then((snapshot) => {
-    console.log(snapshot);
-    //if the day fetched from DB is in the duration, then add to array
-    snapshot.forEach((childSnapshot) => {
-      const date = childSnapshot.key;
-      const count = childSnapshot.val().count;
-      console.log(count);
+  return new Promise((resolve, reject) => {
+    console.log("fetchAppropriateActivity");
+    let activities = [];
+    const ref = database.ref('users/' + uid + '/result');
+    const days = duration.days();
+
+    const startDate = moment(targetDate - duration);
+
+    console.log("EndDate: ", targetDate.format(DATE_YMD_FORMAT));
+    console.log("StartDate: ", startDate.format(DATE_YMD_FORMAT));
+
+    ref.once("value").then((snapshot) => {
+      console.log(snapshot);
+      //if the day fetched from DB is in the duration, then add to array
+      snapshot.forEach((childSnapshot) => {
+        const date = childSnapshot.key;
+        const count = childSnapshot.val().count;
+        //if the date is between startDate and endDate, then push this.
+        console.log("date: ", date);
+        console.log("count:", count);
+        const fetchedDate = moment(date);
+        if ( startDate <= fetchedDate <= targetDate) {
+          const result = {
+            date: date,
+            count: count
+          };
+          activities.push(result);
+        }
+      });
+    }).then(() => {
+      console.log(activities);
+      resolve(activities);
+    }).catch((err) => {
+      console.error(err);
+      reject(err);
     });
-    console.log(activities);
-  }).catch((err) => {
   });
 }
