be545ace25813cbec49b63ee42f8302415f59458
osamtimizer <eaten.jb@gmail.com>
2018-04-25 17:13:43 +0900

    Add user delete GUI

--- public/javascripts/settings.js
+++ public/javascripts/settings.js
@@ -11,11 +11,49 @@ firebase.initializeApp(config);
 const auth = firebase.auth();
 
 $(() => {
-  $("button#submit").click((event) => {
-    $(".alert").fadeIn('fast');
+  const modal_delete = $('[data-remodal-id=modal-delete]').remodal();
+
+  $("button#delete-account").click((event) => {
+    modal_delete.open();
   });
 
-  $("a.close").click((event) => {
-    $(".alert").fadeOut('fast');
+  $("button#btn-deleteAccount").on('click', async(event) => {
+    const user = auth.currentUser;
+    if (user) {
+      user.getIdToken(true).then((idToken) => {
+        execDelete(idToken);
+      }).catch((err) => {
+        console.error(err);
+      });
+    }
+  });
+
+  $(document).on('opening', '.remodal-tasks-delete', (event) => {
+      $("button#btn-deleteAccount").prop("disabled", true);
+      $('input#confirmDeleteAccount').val('');
+  });
+
+  $("input#confirmDeleteAccount").on('keyup', (event) => {
+    if ($('input#confirmDeleteAccount').val() === 'DeleteAccount') {
+      $("button#btn-deleteAccount").prop("disabled", false);
+    } else {
+      $("button#btn-deleteAccount").prop("disabled", true);
+    }
   });
 });
+
+const execDelete = (token) => {
+  let form = document.createElement('form');
+  form.method = 'POST';
+  form.action = '/users/delete';
+
+  let input_token = document.createElement('input');
+  input_token.setAttribute('type', 'hidden');
+  input_token.setAttribute('name', 'token');
+  input_token.setAttribute('value', token);
+  form.appendChild(input_token);
+
+  document.body.appendChild(form);
+
+  form.submit();
+}
