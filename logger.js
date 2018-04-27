db401a194e763963069d84124cb7cba7c81cf121
osamtimizer <eaten.jb@gmail.com>
2018-04-27 09:50:16 +0900

    Add routing tests

--- logger.js
+++ logger.js
@@ -1,5 +1,6 @@
 const config = require('config');
 const log4js = require('log4js');
+
 log4js.configure(config.log4js.configure);
 
 module.exports = {
@@ -7,4 +8,3 @@ module.exports = {
   access: log4js.getLogger('access'),
   error: log4js.getLogger('error')
 }
-
--- test/test.js
+++ test/test.js
@@ -1,24 +1,111 @@
 const chai = require('chai');
 const expect = chai.expect;
 const request = require('supertest');
+const listEndpoints = require('express-list-endpoints');
 const app = require('../app');
 
 const user = request.agent(app);
+const fakeAuthenticatedUser = request.agent(app);
+
 
 describe('not-logged in user test', (done) => {
-  it('should return 200 when user is not logged in', (done) => {
+  before((done) => {
+    console.log(listEndpoints(app));
+    done();
+  });
+
+  it('router "/" will return 200 response', (done) => {
+    user.get('/').end((err, res) => {
+      expect(res.statusCode).to.be.equal(200);
+      done();
+    });
+  });
+
+  it('router "/register" will return 200 response', (done) => {
+    user.get('/register').end((err, res) => {
+      expect(res.statusCode).to.be.equal(200);
+      done();
+    });
+  });
+
+  it('router "/login" will return 200 response', (done) => {
     user.get('/login').end((err, res) => {
       expect(res.statusCode).to.be.equal(200);
       done();
     });
   });
 
-  it('should return 302 when user is not logged in', (done) => {
-    user.get('/home').end((err, res) => {
+  it('router "POST:/login" will return 500 response', (done) => {
+    user.post('/login')
+      .send({
+        token: "token"
+      })
+      .end((err, res) => {
+        expect(res.statusCode).to.be.equal(500);
+        done();
+      });
+  });
+
+  it('router "/logout" will return 302 response', (done) => {
+    user.get('/logout').end((err, res) => {
       expect(res.statusCode).to.be.equal(302);
-      expect(res.header['location']).to.be.equal('/login');
-      console.log(res.header['location']);
+      expect(res.header['location']).to.be.equal('/');
       done();
     });
   });
+
+  it('router "POST:/users" will return 500 response', (done) => {
+    user.post('/users')
+      .send({
+        token: "token"
+      })
+      .end((err, res) => {
+        expect(res.statusCode).to.be.equal(500);
+        done();
+      });
+  });
+
+  it('router "/users/delete" will return 500 response', (done) => {
+    user.post('/users/delete')
+      .send({ token: 'token' })
+      .end((err, res) => {
+        expect(res.statusCode).to.be.equal(500);
+        console.log(res.statusCode);
+        done();
+      });
+  });
+
+
+  it('router "/home" will return 302 response', (done) => {
+    user.get('/home').end((err, res) => {
+      expect(res.statusCode).to.be.equal(302);
+      expect(res.header['location']).to.be.equal('/login');
+      done();
+    });
+  });
+
+  it('router "/activity" will return 302 response', (done) => {
+    user.get('/activity').end((err, res) => {
+      expect(res.statusCode).to.be.equal(302);
+      expect(res.header['location']).to.be.equal('/login');
+      done();
+    });
+  });
+
+  it('router "/settings" will return 302 response', (done) => {
+    user.get('/settings').end((err, res) => {
+      expect(res.statusCode).to.be.equal(302);
+      expect(res.header['location']).to.be.equal('/login');
+      done();
+    });
+  });
+
+  it('router "/task_management" will return 302 response', (done) => {
+    user.get('/task_management').end((err, res) => {
+      expect(res.statusCode).to.be.equal(302);
+      expect(res.header['location']).to.be.equal('/login');
+      done();
+    });
+  });
+
 });
