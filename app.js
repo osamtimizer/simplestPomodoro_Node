806e10980cff956987545cd8677723c8c5ee40e4
osamtimizer <eaten.jb@gmail.com>
2018-05-29 14:01:25 +0900

    remove cookie-parser because this module is unused by express-session

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('./logger');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
@@ -34,7 +33,6 @@ app.set('view engine', 'pug');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser('secret'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //TODO:PRODUCTION:set SESSION_SECRET value in env
