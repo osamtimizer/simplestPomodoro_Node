dd61771315fad669be60f2a72189c0998c17c898
osamtimizer <eaten.jb@gmail.com>
2018-06-05 15:26:05 +0900

    revert firebase version because it seems this change will break my code..

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,4 +1,6 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
 import moment from 'moment';
 import 'jquery-confirm';
 
